5431d355969eab0d3811c35b6a77cbef7775236c
yaburi <nathan.au@hotmail.com>
2019-05-28 21:43:22 +1000

    completed tutorial: reactjs building an interface

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import AddAppointment from './AddAppointment';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-import { without } from 'lodash';
+import { without, findIndex } from 'lodash';
 
 class App extends Component {
   constructor() {
@@ -13,12 +13,18 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: '',
       lastIndex: 0
     };
 
     this.addAppointment = this.addAppointment.bind(this);
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
   }
 
   addAppointment(apt) {
@@ -46,6 +52,31 @@ class App extends Component {
     });
   }
 
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  searchApts(query) {
+    this.setState({
+      queryText: query
+    });
+  }
+
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    });
+  }
+
   componentDidMount() {
     fetch('./data.json')
       .then(response => response.json())
@@ -63,6 +94,40 @@ class App extends Component {
   }
   
   render() {
+
+    let order;
+    let filteredApts = this.state.myAppointments;
+
+    if (this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts = filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() <   // comparing the orderBy property
+          b[this.state.orderBy].toLowerCase()
+      ) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    }).filter(eachItem => {
+      return (
+        eachItem['petName']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase()) 
+        || 
+        eachItem['ownerName']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase())
+        ||
+          eachItem['aptNotes']
+          .toLowerCase()
+          .includes(this.state.queryText.toLowerCase())
+      );
+    });
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
@@ -74,10 +139,16 @@ class App extends Component {
                   toggleForm={this.toggleForm}
                   addAppointment = {this.addAppointment}
                 />
-                <SearchAppointments />
+                <SearchAppointments 
+                  orderBy={this.state.orderBy}
+                  orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
+                  searchApts={this.searchApts}
+                />
                 <ListAppointments 
-                  appointments={this.state.myAppointments} 
+                  appointments={filteredApts} 
                   deleteAppointment={this.deleteAppointment} 
+                  updateInfo={this.updateInfo}
                 />
               </div>
             </div>
